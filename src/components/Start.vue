6d32325edd34208c7e3b2dcb548e6479ec1b8116
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-05-14 12:28:17 +0200

    style start

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <a href="https://fontmeme.com/netflix-font/">
+        <a >
 
             <h2>Cosa vuoi guardare ora?</h2>
             <img src="https://fontmeme.com/permalink/210514/a23b97752ded4f74fe9b7965e1872cce.png" alt="netflix-font" border="0">
@@ -14,8 +14,23 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    section{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
 
+        a{
+            text-decoration: none;
+            img{
+                cursor: pointer;
+                width: 1000px;
+                height: 110px;
+                margin: 15px;
+            }
+        }
+    }
 
 
 </style>
